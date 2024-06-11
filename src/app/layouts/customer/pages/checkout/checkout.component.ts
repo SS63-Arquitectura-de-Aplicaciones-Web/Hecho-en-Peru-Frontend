44b9453e91f298a884646ecc796cd779b8c84bdb
GwynethS <gwynethsegura@gmail.com>
2024-06-10 23:51:34 -0500

    feat: se implementó el checkout component
    Se realizó el maquetado, estilos y funciones para procesar y validar la compra de los productos en la tienda

--- src/app/layouts/customer/pages/checkout/checkout.component.ts
+++ src/app/layouts/customer/pages/checkout/checkout.component.ts
@@ -21,5 +21,96 @@ import { AlertService } from '../../../../core/services/alert.service';
   styleUrl: './checkout.component.scss',
 })
 export class CheckoutComponent {
-  
+  paymentForm: FormGroup;
+  authUser$: Observable<LoginResponse | null>;
+  cartProducts$: Observable<OrderDetailRequest[]>;
+  cartTotal$: Observable<number>;
+
+  subscriptions: Subscription[] = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private store: Store,
+    private authService: AuthService,
+    private shoppingCartService: ShoppingCartService,
+    private orderService: OrderService,
+    private alertService: AlertService,
+    private router: Router
+  ) {
+    this.paymentForm = this.fb.group({
+      phone: this.fb.control('', [
+        Validators.required,
+        Validators.pattern('^\\d{9}$'),
+      ]),
+      verificationCode: this.fb.control('', [
+        Validators.required,
+        Validators.pattern('^\\d{6}$'),
+      ]),
+    });
+
+    this.authUser$ = this.store.select(selectAuthUser);
+    this.cartProducts$ = this.store.select(selectShoppingCartProducts);
+    this.cartTotal$ = this.store.select(selectShoppingCartTotal);
+  }
+
+  onSubmit() {
+    const user: LoginResponse | null = this.authService.getAuthUser();
+
+    if (this.paymentForm.invalid) {
+      this.paymentForm.markAllAsTouched();
+    } else {
+      if (user) {
+        this.alertService
+          .showConfirmAction(
+            '¿Estás seguro que deseas proceder con el pago?',
+            '',
+            'Pagar'
+          )
+          .then((result) => {
+            if (result.isConfirmed) {
+              this.subscriptions.push(
+                this.shoppingCartService.getCartState().subscribe({
+                  next: (state) => {
+                    this.orderService
+                      .createOrder({ ...state, user: user.user })
+                      .subscribe({
+                        next: () => {
+                          this.alertService
+                            .showSuccesActionWaitResponse(
+                              '¡Felicidades!',
+                              'El pago fue procesado correctamente',
+                              'Cerrar'
+                            )
+                            .then(() => {
+                              this.shoppingCartService.onClearCart();
+                              this.router.navigate(['/shop']);
+                            });
+                        },
+                        error: () =>
+                          this.alertService.showError(
+                            'Ups! Ocurrió un problema',
+                            'No se pudo procesar el pago'
+                          ),
+                      });
+                  },
+                  error: () =>
+                    this.alertService.showError(
+                      'Ups! Ocurrió un problema',
+                      'Intente de nuevo más tarde'
+                    ),
+                })
+              );
+            }
+          });
+      }
+    }
+  }
+
+  onCancel() {
+    this.router.navigate(['/shop']);
+  }
+
+  OnDestroy(){
+    this.subscriptions.forEach((suscription) => suscription.unsubscribe());
+  }
 }
--- src/app/layouts/customer/pages/checkout/order.service.ts
+++ src/app/layouts/customer/pages/checkout/order.service.ts
@@ -5,5 +5,14 @@ import { environment } from '../../../../../environments/environment';
 
 @Injectable()
 export class OrderService {
+  constructor(private httpClient: HttpClient) {}
 
+  createOrder(orderRequest: OrderRequest) {
+    console.log(orderRequest);
+    return this.httpClient.post<string>(
+      `${environment.apiURL}order`,
+      orderRequest,
+      { responseType: 'text' as 'json' }
+    );
+  }
 }
