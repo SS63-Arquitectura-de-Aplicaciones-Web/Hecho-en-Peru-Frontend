f1ca5f45b4a8f0ba56e6d159a2e269210854feb5
GwynethS <gwynethsegura@gmail.com>
2024-06-10 23:50:22 -0500

    feat: se implementó el catálogo y detalle del catálogo
    Se realizó el maquetado, estilos y funciones para el catálogo de productos y la información de los productos, junto con las funciones para agregar los productos al carrito

--- src/app/layouts/customer/pages/catalog/catalog.component.ts
+++ src/app/layouts/customer/pages/catalog/catalog.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+import { Product } from '../../../admin/pages/products/models/product';
+import { Subscription, map } from 'rxjs';
+import { Router } from '@angular/router';
+import { ProductsService } from '../../../admin/pages/products/products.service';
+import { Category } from '../../../admin/pages/products/models/category';
+import { Region } from '../../../admin/pages/regions/models/region';
+import { RegionsService } from '../../../admin/pages/regions/regions.service';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { ShoppingCartAction } from '../../../../core/store/shopping-cart/shopping-cart.actions';
+import { OrderDetailRequest } from '../checkout/models/order-detail-request';
+import { ToastService } from '../../../../core/services/toast.service';
 
 @Component({
   selector: 'app-catalog',
@@ -6,6 +18,188 @@ import { Component } from '@angular/core';
   styleUrl: './catalog.component.scss',
 })
 export class CatalogComponent {
+  length = 0;
+  pageSize = 12;
+  pageIndex = 0;
+
+  lengthBestSellers = 0;
+  pageSizeBestSellers = 4;
+  pageIndexBestSellers = 0;
+
+  inputPageNumber = 1;
+
+  productsByPage: Product[] = [];
+
+  filterActive: boolean = false;
+  filterForm: FormGroup;
+  filterProducts: Product[] = [];
+
+  categories: Category[] = [];
+  regions: Region[] = [];
+  products: Product[] = [];
+  bestSellers: Product[] = [];
+  subscriptions: Subscription[] = [];
+
+  constructor(
+    private router: Router,
+    private productsService: ProductsService,
+    private regionService: RegionsService,
+    private toastService: ToastService,
+    private fb: FormBuilder,
+    private store: Store,
+  ) {
+    this.filterForm = this.fb.group({
+      price: this.fb.group({
+        min: this.fb.control(10),
+        max: this.fb.control(1500)
+      }),
+      order: this.fb.control(''),
+      categories: this.fb.array([]),
+      regions: this.fb.array([])
+    });
+  }
+
+  ngOnInit(): void {
+    this.subscriptions.push(
+      this.productsService.getProducts().subscribe({
+        next: (products) => {
+          this.length = products.length;
+          this.products = products;
+          if(!this.filterActive) this.filterProducts = this.products;
+          this.showProductsByPage();
+        },
+      })
+    );
+
+    this.subscriptions.push(
+      this.productsService.getBestSellingProductsUser().subscribe({
+        next: (bestSellers) => {
+          this.lengthBestSellers = bestSellers.length;
+        },
+      })
+    );
+
+    this.getBestSellersByPage();
+    this.getCategories();
+    this.getRegiones();
+  }
+
+  getBestSellersByPage(){
+    this.subscriptions.push(
+      this.productsService
+        .getBestSellingProductsByPageUser(this.pageIndexBestSellers, this.pageSizeBestSellers)
+        .subscribe({
+          next: (bestSellers) => {
+            this.bestSellers = bestSellers;
+          },
+        })
+    );
+  }
+
+  previousBestSellersPage(){
+    if(this.pageIndexBestSellers > 0){
+      this.pageIndexBestSellers--;
+      this.getBestSellersByPage();
+    }
+  }
+
+  nextBestSellersPage(){
+    if(this.pageIndexBestSellers < Math.ceil(this.lengthBestSellers / this.pageSizeBestSellers) - 1){
+      this.pageIndexBestSellers++;
+      this.getBestSellersByPage();
+    }
+  }
+
+  get categoriesArray() {
+    return this.filterForm.get('categories') as FormArray;
+  }
+
+  get regionsArray() {
+    return this.filterForm.get('regions') as FormArray;
+  }
+
+  private addCategoryControl(): void {
+    this.categoriesArray.push(new FormControl(false));
+  }
+
+  private addRegionControl(): void {
+    this.regionsArray.push(new FormControl(false));
+  }
+
+  getCategories(){
+    this.subscriptions.push(
+      this.productsService.getCategories().subscribe({
+        next: (categories) => {
+          this.categories = categories;
+          this.categoriesArray.clear();
+          categories.forEach(() => this.addCategoryControl());
+        }
+      })
+    )
+  }
+
+  getRegiones(){
+    this.subscriptions.push(
+      this.regionService.getRegions().subscribe({
+        next: (regions) => {
+          this.regions = regions;
+          this.regionsArray.clear();
+          regions.forEach(() => this.addRegionControl());
+        }
+      })
+    )
+  }
+
+  showProductsByPage(){
+    const starIndex = this.pageIndex * this.pageSize;
+    const endIndex = starIndex + this.pageSize;
+    this.productsByPage = this.filterProducts.slice(starIndex, endIndex);
+  }
+
+  previousPage() {
+    if (this.pageIndex > 0) {
+      this.pageIndex--;
+      this.showProductsByPage();
+      this.inputPageNumber = this.pageIndex + 1;
+    }
+  }
+
+  changeInputPage() {
+    if (
+      this.inputPageNumber > 0 &&
+      this.inputPageNumber <= Math.ceil(this.length / this.pageSize)
+    ) {
+      this.pageIndex = this.inputPageNumber - 1;
+      this.showProductsByPage();
+    }
+    
+    this.inputPageNumber = this.pageIndex + 1;
+  }
+
+  nextPage() {
+    if (this.pageIndex < Math.ceil(this.length / this.pageSize) - 1) {
+      this.pageIndex++;
+      this.showProductsByPage();
+      this.inputPageNumber = this.pageIndex + 1;
+    }
+  }
+
+  redirectTo(id: string): void {
+    this.router.navigate([`/shop/catalog/${id}`]);
+  }
+
+  onAddProduct(product: Product){
+    const orderDetail: OrderDetailRequest = {
+      product,
+      quantity: 1
+    };
+    
+    this.store.dispatch(ShoppingCartAction.addProduct({orderDetail}));
+    this.toastService.showToast("Se añadió el producto al carrito");
+  }
   
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((suscription) => suscription.unsubscribe());
+  }
 }
 
