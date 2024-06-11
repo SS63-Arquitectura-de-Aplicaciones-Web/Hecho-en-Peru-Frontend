ebd79848a4cf021cdc4810312127775ece35a952
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2024-06-10 22:30:46 -0500

    feat: Se implementaron guards para las rutas de la app
    Se realizó las validaciones para proteger la vista administrativa y la vista de cliente, asi como el formulario de login y la página de perfil de usuario

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,5 +1,10 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../../layouts/customer/pages/auth/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const router = inject(Router);
+  const authService = inject(AuthService);
+
+  return authService.verifyToken() ? router.createUrlTree(['shop', 'user', 'profile']) : true;
 };
--- src/app/core/guards/checkout.guard.ts
+++ src/app/core/guards/checkout.guard.ts
@@ -1,5 +1,16 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { CheckoutService } from '../../layouts/customer/pages/checkout/checkout.service';
 
 export const checkoutGuard: CanActivateFn = (route, state) => {
-  return true;
+  const checkoutService = inject(CheckoutService);
+  const router = inject(Router);
+
+  if (checkoutService.getCheckoutAccess()) {
+    checkoutService.resetCheckoutAccess(); 
+    return true;
+  } else {
+    router.navigate(['/shop']); 
+    return false;
+  }
 };
