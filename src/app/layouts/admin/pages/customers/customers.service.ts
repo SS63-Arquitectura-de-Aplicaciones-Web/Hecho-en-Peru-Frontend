59ef5a605aef3a8fc43f7bd2237669f96a6a4dc7
Alexandra <alexa.dayanna07@gmail.com>
2024-06-10 21:54:46 -0500

    feat: se implementó el registro de usuario
    Se realizó el maquetado del componente, se aplicaron estilos, se crearon las funciones para validar los formularios reactivos y se creó el servicio customer

--- src/app/layouts/admin/pages/customers/customers.service.ts
+++ src/app/layouts/admin/pages/customers/customers.service.ts
@@ -1,6 +1,50 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Customer } from './models/customer';
+import { environment } from '../../../../../environments/environment';
+import { finalize } from 'rxjs';
+import { AuthService } from '../../../customer/pages/auth/auth.service';
+import { UserProfile } from '../../../customer/pages/user/pages/profile/models/user-profile';
+import { OrderDetail } from './pages/order-detail/models/order-detail';
 
 @Injectable()
 export class CustomersService {
-  
+  constructor(
+    private httpClient: HttpClient,
+    private authService: AuthService
+  ) {}
+
+  getCustomers() {
+    return this.httpClient.get<Customer[]>(`${environment.apiURL}users`);
+  }
+
+  getCustomersByPageAdmin(offSet: number, limit: number) {
+    return this.httpClient.get<Customer[]>(`${environment.apiURL}usersByPageModeAdmin?offset=${offSet}&limit=${limit}`);
+  }
+
+  getSearchCustomerByID(id: string) {
+    return this.httpClient.get<Customer>(`${environment.apiURL}user/${id}`);
+  }
+
+  getOrders() {
+    return this.httpClient.get<Customer[]>(`${environment.apiURL}orders`);
+  }
+
+  getSearchOrderDetailsByID(orderId: string, userId: string) {
+    return this.httpClient.get<OrderDetail>(`${environment.apiURL}ordersDetails?orderId=${orderId}&userId=${userId}`);
+  }
+
+  createUser(userData: Customer) {
+    return this.httpClient.post<Customer>(`${environment.apiURL}auth/register`, { ...userData });
+  }
+
+  updateUser(userProfile: UserProfile, id: string) {
+    return this.httpClient.put<Customer>(`${environment.apiURL}userUpdate/${id}`, userProfile);
+  }
+
+  deleteUser(id: string) {
+    return this.httpClient
+      .delete(`${environment.apiURL}user/${id}`, { responseType: 'text' })
+      .pipe(finalize(() => this.authService.logOut()));
+  }
 }
