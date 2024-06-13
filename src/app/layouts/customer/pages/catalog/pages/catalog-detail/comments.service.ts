e37a02cb14936efd18ee04bd50e6b208be7a7aa3
Alexandra <alexa.dayanna07@gmail.com>
2024-06-12 21:49:51 -0500

    feat: se implementó el add comment
    Se realizó la llamada a la api para insertar un comentario en la base de datos

--- src/app/layouts/customer/pages/catalog/pages/catalog-detail/comments.service.ts
+++ src/app/layouts/customer/pages/catalog/pages/catalog-detail/comments.service.ts
@@ -1,6 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../../../../../environments/environment';
+import { Comment } from './models/comment';
+import { mergeMap } from 'rxjs';
 
 @Injectable()
 export class CommentsService {
-  
+  constructor(private httpClient: HttpClient) {}
+
+  addComment(data: Comment, offSet: number, limit: number) {
+    return this.httpClient
+      .post<Comment>(`${environment.apiURL}comment`, data)
+      .pipe(
+        mergeMap(() =>
+          this.getCommentsByPageByProduct(data.product.id, offSet, limit)
+        )
+      );
+  }
 }
