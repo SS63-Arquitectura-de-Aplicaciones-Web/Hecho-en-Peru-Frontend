1a54e2b8384c581870a9f8420a2084c60edec7b5
Alexandra <alexa.dayanna07@gmail.com>
2024-06-13 16:34:22 -0500

    feat: se implementó el view order details by client
    Se realizó el maquetado y estilos del componente order-detail y se enlazaron las funciones en el customers service para obtener los datos de las órdenes de compra de un determinado cliente

--- src/app/layouts/admin/pages/customers/customers.service.ts
+++ src/app/layouts/admin/pages/customers/customers.service.ts
@@ -18,10 +18,6 @@ export class CustomersService {
     return this.httpClient.get<Customer[]>(`${environment.apiURL}users`);
   }
 
-  getCustomersByPageAdmin(offSet: number, limit: number) {
-    return this.httpClient.get<Customer[]>(`${environment.apiURL}usersByPageModeAdmin?offset=${offSet}&limit=${limit}`);
-  }
-
   getSearchCustomerByID(id: string) {
     return this.httpClient.get<Customer>(`${environment.apiURL}user/${id}`);
   }
@@ -30,8 +26,12 @@ export class CustomersService {
     return this.httpClient.get<Customer[]>(`${environment.apiURL}orders`);
   }
 
-  getSearchOrderDetailsByID(orderId: string, userId: string) {
-    return this.httpClient.get<OrderDetail>(`${environment.apiURL}ordersDetails?orderId=${orderId}&userId=${userId}`);
+  getOrderDetailByUserIdByPageAdmin(customerId: string, offset: number, limit: number) {
+    return this.httpClient.get<OrderDetail[]>(`${environment.apiURL}orderDetailByUserId?userId=${customerId}&offset=${offset}&limit=${limit}`);
+  }
+
+  getAllOrderDetailsByUserIDAdmin(customerId: string) {
+    return this.httpClient.get<OrderDetail[]>(`${environment.apiURL}allOrderDetailsByUserId?userId=${customerId}`);
   }
 
   createUser(userData: Customer) {
