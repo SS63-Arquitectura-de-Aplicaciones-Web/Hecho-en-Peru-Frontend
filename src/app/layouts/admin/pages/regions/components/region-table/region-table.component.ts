296bd5fc81666b5336ba6874459834983b40f675
Alexandra <alexa.dayanna07@gmail.com>
2024-06-16 02:05:30 -0500

    feat: se implementó el view regions
    Se realizó el maquetado y estilos del componente regions y se enlazaron las funciones en el regions service para obtener los datos de las regiones.
    
    Co-Authored-By: Alvaro Alison Patricio Acuña <136278457+AlvaroAlison07@users.noreply.github.com>

--- src/app/layouts/admin/pages/regions/components/region-table/region-table.component.ts
+++ src/app/layouts/admin/pages/regions/components/region-table/region-table.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { Region } from '../../models/region';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-region-table',
   templateUrl: './region-table.component.html',
   styleUrl: './region-table.component.scss',
 })
-export class RegionTableComponent {
-  
+export class RegionTableComponent implements AfterViewInit {
+  @Input()
+  set dataSource(dataSource: MatTableDataSource<Region>) {
+    this._dataSource = dataSource;
+    if (this.paginator) {
+      this._dataSource.paginator = this.paginator;
+    }
+  }
+
+  get dataSource(): MatTableDataSource<Region> {
+    return this._dataSource;
+  }
+
+  @Output()
+  editRegion = new EventEmitter<Region>();
+
+  displayedColumns: string[] = [
+    'id',
+    'name',
+    'history',
+    'sitesIntroduction',
+    'craftsmenIntroduction',
+    'actions',
+  ];
+
+  _dataSource = new MatTableDataSource<Region>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private router: Router) {}
+
+  ngAfterViewInit() {
+    if (this.paginator) {
+      this._dataSource.paginator = this.paginator;
+    }
+  }
+
+  onViewTouristSites(id: string) {
+    this.router.navigate([`/admin/regions/${id}`]);
+  }
 }
--- src/app/layouts/admin/pages/regions/regions.component.ts
+++ src/app/layouts/admin/pages/regions/regions.component.ts
@@ -1,10 +1,138 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Region } from './models/region';
+import { MatDialog } from '@angular/material/dialog';
+import { RegionsService } from './regions.service';
+import { RegionDialogComponent } from './components/region-dialog/region-dialog.component';
+import { FormBuilder, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { ToastService } from '../../../../core/services/toast.service';
+import { AlertService } from '../../../../core/services/alert.service';
 
 @Component({
   selector: 'app-regions',
   templateUrl: './regions.component.html',
   styleUrl: './regions.component.scss',
 })
-export class RegionsComponent {
-  
+export class RegionsComponent implements OnInit, OnDestroy {
+  regionSearchForm: FormGroup;
+  regions: Region[] = [];
+  dataSource = new MatTableDataSource<Region>();
+
+  searchAttempted: boolean = false;
+
+  subscriptions: Subscription[] = [];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  @ViewChild(FormGroupDirective)
+  private regionSearchFormDir!: FormGroupDirective;
+
+  constructor(
+    private fb: FormBuilder,
+    private regionsService: RegionsService,
+    private matDialog: MatDialog,
+    private alertService: AlertService,
+    private toastService: ToastService,
+  ) {
+    this.regionSearchForm = this.fb.group({
+      name: this.fb.control('', [ Validators.required, Validators.pattern('^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]*$')]),
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadRegions();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+  }
+
+  loadRegions(): void {
+    const subscription = this.regionsService
+      .getAllRegions()
+      .subscribe({
+        next: (regions) => {
+          this.searchAttempted = false;
+          this.regions = regions || [];
+          this.dataSource.data = this.regions;
+        },
+        error: () => {
+          this.dataSource.data = [];
+          this.alertService.showError(
+            'Ups! Ocurrió un error',
+            'No se pudieron cargar los datos correctamente'
+          )
+        }
+      });
+    this.subscriptions.push(subscription);
+  }
+
+  onSearch(): void {
+    if (this.regionSearchForm.invalid) {
+      this.regionSearchForm.markAllAsTouched();
+    } else {
+      const subscription = this.regionsService
+        .getSearchRegionByName(this.regionSearchForm.value.name)
+        .subscribe({
+          next: (region: Region[]) => {
+            this.searchAttempted = false;
+            this.regions = region;
+            this.dataSource.data = this.regions;
+          },
+          error: () => {
+            this.dataSource.data = [];
+            this.searchAttempted = true;
+          }
+        });
+      this.subscriptions.push(subscription);
+    }
+  }
+
+  onClean(): void {
+    this.searchAttempted = false;
+    this.regionSearchFormDir.resetForm();
+    this.loadRegions();
+  }
+
+  onCreateRegion(): void {
+    this.matDialog
+      .open(RegionDialogComponent)
+      .afterClosed()
+      .subscribe((result) => {
+        if (result) {
+          const { regionData, image } = result;
+          this.regionsService
+            .addRegions(regionData, image)
+            .subscribe({
+              next: () => {
+                this.loadRegions(),
+                this.toastService.showToast("Se añadió la región correctamente");
+              },
+            });
+        }}
+      );
+  }
+
+  onEditRegion(region: Region): void {
+    this.matDialog
+      .open(RegionDialogComponent, { data: region })
+      .afterClosed()
+      .subscribe({
+        next: (result) => {
+          if (result) {
+            const { regionData, image } = result;
+            this.regionsService
+              .updateRegions(region.id, regionData, image)
+              .subscribe({
+                next: () => {
+                  this.loadRegions(),
+                  this.toastService.showToast("Se actualizó la región correctamente");
+                },
+              });
+          }
+        },
+      });
+  }
 }
