0ce2bf04344d5ed1294889c4b492c1fbef9210d4
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2024-06-17 19:58:05 -0500

    feat: se implementó el add tourist sites
    Se realizó la llamada a la api para agregar los lugares turisticos y se agregó el maquetado y los estilos del tourist-site-dialog

--- src/app/layouts/admin/pages/regions/pages/tourist-sites/tourist-sites.service.ts
+++ src/app/layouts/admin/pages/regions/pages/tourist-sites/tourist-sites.service.ts
@@ -1,6 +1,53 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { TouristSite } from './models/tourist-site';
+import { environment } from '../../../../../../../environments/environment';
+import { catchError, finalize, of } from 'rxjs';
+import { LoadingService } from '../../../../../../core/services/loading.service';
 
 @Injectable()
 export class TouristSitesService {
-  
+  constructor(
+    private httpClient: HttpClient,
+    private loadingService: LoadingService
+  ) {}
+
+  getTouristSites() {
+    this.loadingService.setIsLoading(true);
+    return this.httpClient
+      .get<TouristSite[]>(`${environment.apiURL}touristSites`)
+      .pipe(finalize(() => this.loadingService.setIsLoading(false)));
+  }
+
+  getAllTouristSitesByRegionId(regionId: string) {
+    this.loadingService.setIsLoading(true);
+    return this.httpClient
+      .get<TouristSite[]>(`${environment.apiURL}allTouristSitesByRegionId?regionId=${regionId}`)
+      .pipe(finalize(() => this.loadingService.setIsLoading(false)));
+  }
+
+  getTouristSitesByRegion(regionId: string) {
+    this.loadingService.setIsLoading(true);
+    return this.httpClient
+      .get<TouristSite[]>(`${environment.apiURL}touristSitesByRegion/${regionId}`)
+      .pipe(finalize(() => this.loadingService.setIsLoading(false)));
+  }
+
+  addTouristSites(data: TouristSite, file: File) {
+    this.loadingService.setIsLoading(true);
+    const formData: FormData = new FormData();
+    formData.append('touristSiteDTO', new Blob([JSON.stringify(data)], { type: "application/json" }));
+    if (file) {
+      formData.append('file', file, file.name);
+    }
+    return this.httpClient.post(`${environment.apiURL}touristSite`, formData)
+    .pipe(
+      catchError((err) => {
+        console.error('Failed to add tourist site', err);
+        return of({ err });
+      }),
+      finalize(() => this.loadingService.setIsLoading(false))
+    );
+  }
+
 }
